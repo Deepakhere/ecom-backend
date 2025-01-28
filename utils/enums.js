916dbc83888189357b5fc3e675ef1b7e3674ef24
deepak.gupta <deepak.gupta@rapidops.co>
2025-01-28 20:16:14 +0530

    add store url mongo

--- utils/enums.js
+++ utils/enums.js
@@ -1,3 +1,5 @@
+import MongoStore from "connect-mongo";
+
 export const CORS_OPTIONS = {
   origin: "http://localhost:4000",
   credentials: true,
@@ -8,6 +10,10 @@ export const SESSION_OPTIONS = {
   secret: process.env.SESSION_SECRET || "secret-key-ecommerce",
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl:
+      "mongodb+srv://deepakgupta:mongodbpaasword@cluster0.xnwuo.mongodb.net/",
+  }),
   cookie: {
     secure: false,
     // httpOnly: false,
